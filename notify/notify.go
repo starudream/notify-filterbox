d69860206f1eb56b1f1bb66fbd477ca64872f615
sheng.wang <justwangsheng@gmail.com>
2024-10-30 12:47:39 +0800

    refactor: use node-notifier

--- notify/notify.go
+++ notify/notify.go
@@ -1,5 +1,13 @@
 package notify
 
+import (
+	"os/exec"
+	"strings"
+
+	"github.com/starudream/go-lib/core/v2/config"
+	"github.com/starudream/go-lib/core/v2/slog"
+)
+
 type Params struct {
 	Id       string
 	Title    string
@@ -14,5 +22,31 @@ type Result struct {
 }
 
 func Notify(p Params) (Result, error) {
-	return notify(p)
+	res := Result{Id: p.Id}
+
+	args := []string{"--message", p.Message, "--timeout", "30"}
+	if p.Title != "" {
+		args = append(args, "--title", p.Title)
+	}
+	if p.SubTitle != "" {
+		args = append(args, "--subtitle", p.SubTitle)
+	}
+
+	path := "npx node-notifier-cli notify"
+	if config.Exists("notify.path") {
+		path = config.Get("notify.path").String()
+	}
+
+	ss := strings.Split(path, " ")
+	if len(ss) > 1 {
+		path = ss[0]
+		args = append(ss[1:], args...)
+	}
+
+	slog.Debug("exec: %s %v", path, strings.Join(args, " "))
+
+	bs, err := exec.Command(path, args...).Output()
+	res.Out = string(bs)
+
+	return res, err
 }
